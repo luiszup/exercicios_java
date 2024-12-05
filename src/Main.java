cc7e590faf91f33ee404e98b16ebe660967caa3a
Luís Cláudio Marques Maynard do Lago <lcmmlago@gmail.com>
2024-12-05 10:04:34 -0300

    Adiciona exercicio de soma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite um número: ");
+        int n = input.nextInt();
+
+        int soma = 0;
+
+        for (int i = 0; i <= n; i++) {
+            soma = soma + i;
+        }
+
+        System.out.println("Soma = " + soma);
+
+        input.close();
     }
 }
